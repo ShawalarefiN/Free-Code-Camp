9a7a280ab2c97daf1c5ce3c778f6944642525de1
Shawal Arefin <shawalarefin132@gmail.com>
2026-05-24 09:42:05 -0400

    starting

---
+++ python/test.py
@@ -0,0 +1,13 @@
+string = 'Hello world'
+my_str_3 = """Multiline
+
+string"""
+
+print("hello" in string)
+print("Hello" in string)
+print(string[0])
+print(len(string))
+print(my_str_3)
+name01 = 'shawal'
+name02 = 'arefin'
+print(f'my name is {name01} {name02}')
